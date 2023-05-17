c5b94575b7e3c476575d007e6cc7c43eee2467c5
Uminosachi <49424133+Uminosachi@users.noreply.github.com>
2023-05-18 08:38:02 +0900

    Add optional parameter

--- ocalm_app.py
+++ ocalm_app.py
@@ -40,7 +40,7 @@ def clear_cache():
     gc.collect()
     torch_gc()
 
-def open_calm_inference(open_calm_model_id, input_text_box, max_new_tokens, temperature):
+def open_calm_inference(open_calm_model_id, input_text_box, max_new_tokens, temperature, top_k, top_p, repetition_penalty):
     clear_cache()
     
     dwonload_result = download_model(open_calm_model_id, local_files_only=True)
@@ -67,14 +67,18 @@ def open_calm_inference(open_calm_model_id, input_text_box, max_new_tokens, temp
             max_new_tokens=max_new_tokens,
             do_sample=True,
             temperature=temperature,
+            top_k=top_k,
+            top_p=top_p,
+            repetition_penalty=float(repetition_penalty),
             pad_token_id=tokenizer.pad_token_id,
         )
     t2 = time.time()
     elapsed_time = t2-t1
     print(f"Generation time: {elapsed_time} seconds")
 
-    print("Generation completed.")
     output = tokenizer.decode(tokens[0], skip_special_tokens=True)
+    print("Generation completed.")
+
     return output, f"Generation time: {elapsed_time} seconds"
 
 def on_ui_tabs():
@@ -99,12 +103,18 @@ def on_ui_tabs():
                 
                 input_text_box = gr.Textbox(label="Input text", elem_id="input_text_box", placeholder="Input text here", max_lines=16, show_label=True)
                 max_new_tokens = gr.Slider(minimum=1, maximum=512, step=1, value=64, label="Max new tokens", elem_id="max_new_tokens")
-                temperature = gr.Slider(minimum=0.1, maximum=1.0, step=0.1, value=0.7, label="Temperature", elem_id="temperature")
+                with gr.Accordion("Advanced options", open=False):
+                    temperature = gr.Slider(minimum=0.1, maximum=1.0, step=0.1, value=0.7, label="Temperature", elem_id="temperature")
+                    top_k = gr.Slider(minimum=1, maximum=100, step=1, value=50, label="Top k", elem_id="top_k")
+                    top_p = gr.Slider(minimum=0.1, maximum=1.0, step=0.1, value=1.0, label="Top p", elem_id="top_p")
+                    repetition_penalty = gr.Slider(minimum=1.0, maximum=10.0, step=0.5, value=1.0, label="Repetition penalty", elem_id="repetition_penalty")
+
                 generate_btn = gr.Button("Generate", elem_id="generate_btn")
                 output_text_box = gr.Textbox(label="Output text", elem_id="output_text_box", placeholder="Output text here", show_label=True, interactive=False)
-
+            
             download_model_btn.click(download_model, inputs=[open_calm_model_id], outputs=[status_text])
-            generate_btn.click(open_calm_inference, inputs=[open_calm_model_id, input_text_box, max_new_tokens, temperature], outputs=[output_text_box, status_text])
+            generate_inputs = [open_calm_model_id, input_text_box, max_new_tokens, temperature, top_k, top_p, repetition_penalty]
+            generate_btn.click(open_calm_inference, inputs=generate_inputs, outputs=[output_text_box, status_text])
 
     return [(open_calm_interface, "Open CALM", "open_calm")]
 
