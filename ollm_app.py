2c84186bffd73b3ba2b1e25a408f537b04857023
Uminosachi <49424133+Uminosachi@users.noreply.github.com>
2024-05-10 09:18:04 +0900

    Change step of top_p slider to increments of 0.05

--- ollm_app.py
+++ ollm_app.py
@@ -249,7 +249,7 @@ def on_ui_tabs():
                         translate_chk = gr.Checkbox(label="Translate (ja->en/en->ja)", elem_id="translate_chk", value=False, show_label=True)
                         temperature = gr.Slider(minimum=0.1, maximum=1.0, step=0.1, value=0.7, label="Temperature", elem_id="temperature")
                         top_k = gr.Slider(minimum=1, maximum=200, step=1, value=50, label="Top k", elem_id="top_k")
-                        top_p = gr.Slider(minimum=0.1, maximum=1.0, step=0.1, value=1.0, label="Top p", elem_id="top_p")
+                        top_p = gr.Slider(minimum=0.1, maximum=1.0, step=0.05, value=1.0, label="Top p", elem_id="top_p")
                         repetition_penalty = gr.Slider(minimum=1.0, maximum=10.0, step=0.1, value=1.1, label="Repetition penalty", elem_id="repetition_penalty")
                 with gr.Row():
                     generate_btn = gr.Button("Generate", elem_id="generate_btn")
