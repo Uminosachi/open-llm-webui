21fd4644d5aa437946076d0b8e9e7be99deecbff
Uminosachi <49424133+Uminosachi@users.noreply.github.com>
2024-06-20 16:48:57 +0900

    Add test class for TinyLLaVA model

--- model_manager/llava.py
+++ model_manager/llava.py
@@ -3,7 +3,8 @@ import platform
 
 import torch
 from huggingface_hub import snapshot_download
-from transformers import BitsAndBytesConfig, LlavaNextForConditionalGeneration, LlavaNextProcessor
+from transformers import (AutoModelForCausalLM, AutoProcessor, BitsAndBytesConfig,
+                          LlavaNextForConditionalGeneration, LlavaNextProcessor)
 
 from cache_manager import clear_cache_decorator
 from custom_logging import ollm_logging
@@ -87,6 +88,53 @@ class LlavaVicunaModel(LLMConfig):
                 offload_buffers=True,
             ),
             tokenizer_kwargs=dict(
+            ),
+            tokenizer_input_kwargs=dict(
+                return_tensors="pt",
+            ),
+            tokenizer_decode_kwargs=dict(
+                skip_special_tokens=True,
+            ),
+            output_text_only=True,
+            multimodal_image=True,
+        )
+
+    @replace_br_and_code
+    @clear_cache_decorator
+    def create_prompt(self, chatbot, ollm_model_id, input_text_box, rag_text_box, tokenizer=None):
+        prompt = self.prompt_template.format(prompt=input_text_box)
+        return prompt
+
+    @clear_cache_decorator
+    def get_generate_kwargs(self, tokenizer, inputs, ollm_model_id, generate_params):
+        generate_kwargs = super().get_generate_kwargs(tokenizer, inputs, ollm_model_id, generate_params)
+        return generate_kwargs
+
+    @clear_cache_decorator
+    def retreive_output_text(self, input_text, output_text, ollm_model_id, tokenizer=None):
+        return output_text
+
+
+@register_model("tinyllava")
+class TinyLLaVAModel(LLMConfig):
+    include_name: str = "TinyLLaVA"
+
+    prompt_template = ("User: <image>\n{prompt} Assistant:")
+    # quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16)
+
+    def __init__(self):
+        super().__init__(
+            model_class=AutoModelForCausalLM,
+            tokenizer_class=AutoProcessor,
+            model_kwargs=dict(
+                torch_dtype=torch.float16,
+                # quantization_config=self.quantization_config,
+                offload_buffers=True,
+                trust_remote_code=True,
+            ),
+            model_generate_name="chat",
+            tokenizer_kwargs=dict(
+                pretrained_model_name_or_path="google/siglip-so400m-patch14-384",
             ),
             tokenizer_input_kwargs=dict(
                 return_tensors="pt",
@@ -205,6 +253,7 @@ def get_llava_ollm_model_ids():
     llava_ollm_model_ids = [
         "llava-hf/llava-v1.6-mistral-7b-hf",
         "llava-hf/llava-v1.6-vicuna-7b-hf",
+        "tinyllava/TinyLLaVA-Phi-2-SigLIP-3.1B",
     ]
 
     return llava_ollm_model_ids
